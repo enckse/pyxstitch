18b18f6d67d6a9d32c5ebeb4f6022d36f3ee636c
Sean Enck <enckse@gmail.com>
2017-06-27 23:24:10 -0400

    can do Hello, World in python

--- main.py
+++ main.py
@@ -10,33 +10,5 @@ def main():
 
 if __name__ == '__main__':
     main()"""
-code = """
-def aABCDEFG
-    HIJK
-
-LMNOPQRS
-
-TUVWXYZ
-AA AAAAAAAA BA AAWWWWA!
-    AAAAAA
-    W
-"""
-
-#code = ""
-#import string
-#for st in string.printable:
-#    code += "st"
-max_line = 0
-lines = code.split("\n")
-for line in lines:
-    if len(line) > max_line:
-        max_line = len(line)
-
-spaced = []
-for line in lines:
-    padded = line
-    while len(padded) < max_line:
-        padded = padded + " "
-    spaced.append(padded)
 
 highlight(code, PythonLexer(), fmt.CrossStitchFormatter(style='monokai'))
