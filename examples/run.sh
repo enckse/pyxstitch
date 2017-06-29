a667e596c9a9ce5f57e4d0fc665dff673a631520
Sean Enck <enckse@gmail.com>
2017-06-29 07:00:14 -0400

    do all ascii as a flat file

--- examples/run.sh
+++ examples/run.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 ASCII="../bin/ascii.txt"
 export PYTHONPATH="../:$PYTHONPATH"
-for f in $(find . -type f | grep "hello_world" | grep -v "png$"); do
-    pyxstitch --file $f
+for f in $(find . -type f | grep -v "run.sh"); do
+    pyxstitch --file $f --output ../bin/$(basename $f).png
     if [ $? -ne 0 ]; then
         echo "processing error $f"
         exit 1
     fi
 done
-
-python -c """import string; 
-
-for ch in string.printable:
-    print(ch)
-""" > $ASCII
-if [ $? -ne 0 ]; then
-    echo "ascii test failed"
-    exit
-fi
-pyxstitch --file $ASCII
