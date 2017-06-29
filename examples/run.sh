1b3c23ede1b6a323054ff0e17632d188434f7641
Sean Enck <enckse@gmail.com>
2017-06-29 16:50:28 -0400

    verified/checked output pngs

--- examples/run.sh
+++ examples/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 ASCII="../bin/ascii.txt"
 export PYTHONPATH="../:$PYTHONPATH"
-for f in $(find . -type f | grep -v "run.sh"); do
-    pyxstitch --file $f --output ../bin/$(basename $f).png
+for f in $(find . -maxdepth 1 -type f | grep -v "run.sh"); do
+    file_name=$(basename $f).png
+    bin_file=../bin/$file_name
+    pyxstitch --file $f --output $bin_file
     if [ $? -ne 0 ]; then
         echo "processing error $f"
         exit 1
     fi
+    diff $bin_file outputs/$file_name
 done
