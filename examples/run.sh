94037fb9d27749acd4f62017d97e134cfe32efe4
Sean Enck <enckse@gmail.com>
2017-06-28 21:41:34 -0400

    also test ascii

--- examples/run.sh
+++ examples/run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+ASCII="../bin/ascii.txt"
 export PYTHONPATH="../:$PYTHONPATH"
-for f in $(find . -type f); do
+for f in $(find . -type f | grep "^hello_world"); do
     python ../main.py --file $f
     if [ $? -ne 0 ]; then
+        echo "processing error $f"
         exit 1
     fi
 done
+
+python -c """import string; 
+
+for ch in string.printable:
+    print(ch)
+""" > $ASCII
+if [ $? -ne 0 ]; then
+    echo "ascii test failed"
+    exit
+fi
+python ../main.py --file $ASCII
