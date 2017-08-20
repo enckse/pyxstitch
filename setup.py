0e6d7d1cc69222df8016fce229c4a1bf85130d21
Sean Enck <enckse@gmail.com>
2017-08-20 10:07:53 -0400

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyxstitch',
-    version="0.1.2",
+    version="1.1.0",
     description='Convert source code to cross stitch patterns',
     url='https://github.com/enckse/pyxstitch',
     license='MIT',
