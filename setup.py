6c8f808d69dca94b4ceb779ec4fa5c36277535f9
Sean Enck <enckse@gmail.com>
2017-08-29 08:33:09 -0400

    setup.py adjusted

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@
 """Setup for pyxstitch."""
 
 from setuptools import setup, find_packages
+import os
+
+__pkg_name__ = "pyxstitch"
+version = ''
+with open(os.path.join(__pkg_name__, 'VERSION')) as v_file:
+    version = v_file.read().strip()
 
 setup(
-    name='pyxstitch',
-    version="1.1.0",
+    name=__pkg_name__,
+    version=version,
     description='Convert source code to cross stitch patterns',
     url='https://github.com/enckse/pyxstitch',
     license='MIT',
-    packages=['pyxstitch'],
+    packages=[__pkg_name__],
     install_requires=['webcolors', 'pygments', 'pillow'],
     entry_points={
         'console_scripts': [
