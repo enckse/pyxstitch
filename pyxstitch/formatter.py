0d92dba7a775939d59a34d8ec01b203744de0d8c
Sean Enck <enckse@gmail.com>
2017-09-14 18:42:20 -0400

    cleaning up

--- pyxstitch/formatter.py
+++ pyxstitch/formatter.py
@@ -7,6 +7,7 @@ Takes a text stream and converts to a cross stitch output (HTML).
 
 from pygments.formatter import Formatter
 import pyxstitch.font as ft
+import pyxstitch.formatter_layout as fl
 import pyxstitch.hex as hu
 import pyxstitch.symbols as sym
 import pyxstitch.version as vers
@@ -15,61 +16,6 @@ from pyxstitch.config import Config
 from pyxstitch.floss import Floss
 from math import floor
 from enum import Enum
-
-
-class Style(object):
-    """Output/legend formatting."""
-
-    def __init__(self, dmc, symbol, original_hex):
-        """Init the instance."""
-        self.dmc = dmc
-        self.symbol = symbol
-        self._raw_hex = original_hex
-
-    def save(self):
-        """Save to metadata format."""
-        return [self.dmc.name, self.symbol, self._raw_hex]
-
-
-class Legend(object):
-    """Legend object."""
-
-    def __init__(self):
-        """Init the instance."""
-        self._stitches = {}
-        self._entries = []
-
-    def add_raw_stitch(self, dmc):
-        """Add raw stitch edge."""
-        raw_color = dmc.name
-        if raw_color not in self._stitches:
-            self._stitches[raw_color] = 0
-        self._stitches[raw_color] += 1
-
-    def add(self, style):
-        """New legend entry."""
-        self._entries.append(style)
-
-    def build(self):
-        """Build output legend."""
-        headers = []
-        cols = ("dmc", "symbol", "edges", "floss")
-        delim = []
-        for col in cols:
-            delim.append("---")
-        headers.append(cols)
-        headers.append(tuple(delim))
-        output = []
-        for item in self._entries:
-            output.append(("{} ({})".format(item.dmc.name, item.dmc.rgb),
-                           item.symbol,
-                           self._stitches[item.dmc.name],
-                           item.dmc.floss_number))
-        return ["{:>40} {:>7} {:>6} {:>6}".format(x[0],
-                                                  x[1],
-                                                  x[2],
-                                                  x[3])
-                for x in headers + sorted(set(output))]
 
 
 class CrossStitchFormatter(Formatter):
@@ -132,9 +78,9 @@ class CrossStitchFormatter(Formatter):
             use_color = self._colors[token]
         use_hex = hu.to_hex(use_color)
         dmc = self.floss.lookup(use_hex)
-        return Style(dmc,
-                     self.symbol_generator.next(dmc.name),
-                     use_color)
+        return fl.Style(dmc,
+                        self.symbol_generator.next(dmc.name),
+                        use_color)
 
     def _new_entry(self, ch, style):
         """new entry to process."""
@@ -228,7 +174,7 @@ class CrossStitchFormatter(Formatter):
         self._writer.text((offset, offset), vers.__version__, self._symbols)
         y = -1
         lines = []
-        lgd = Legend()
+        lgd = fl.Legend()
         for entry in entries:
             for height in self.font_factory.height():
                 y += 1
