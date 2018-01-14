b1577e2623398dc81f674e9ecba8c0c938db9bb9
Sean Enck <enckse@gmail.com>
2018-01-14 10:19:24 -0500

    changing to use rst

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ __pkg_name__ = "pyxstitch"
 with open(os.path.join(__pkg_name__, "version.py")) as v_file:
     exec(v_file.read())
 
+long_description = ""
+with open("README.rst", 'r') as f:
+    long_description = f.read()
+
 setup(
     author="Sean Enck",
     author_email="enckse@gmail.com",
     name=__pkg_name__,
     version=__version__,
     description='Convert source code to cross stitch patterns',
+    long_description=long_description,
     url='https://github.com/enckse/pyxstitch',
     license='MIT',
     python_requires='>=3',
